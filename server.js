a108a68b24c3d29afe58720af41d1ce774b0e005
Wesley <wesleymcginn1@gmail.com>
2024-09-02 23:26:52 -0400

    Condense websocket and page into one server.

--- server.js
+++ server.js
@@ -7,41 +7,35 @@ const clients = new Set();
 var DATA = '';
 
 http.createServer((req, res) => {
-  fs.readFile("./Desktop/page.html", function (err, data) {
-    if (err) {
-      console.log("404: HTML file does not exist!");
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("<body style='text-align: center'><h1><b><u>404 Error</u></b></h1><h2>Page not found.  :(</h2></body>");
-    } else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    }
-  });
-}).listen(80);
-
-http.createServer((req, res) => {
-  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onSocketConnect);
-}).listen(81);
-
-function onSocketConnect(ws) {
-
-  clients.add(ws);
-  console.log("1 Person joined the document.");
-  for(let client of clients) {
-    client.send(DATA);
+  if (req.url != "/connect") {
+    fs.readFile("./Desktop/page.html", function (err, data) {
+      if (err) {
+        console.log("404: HTML file does not exist!");
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        return res.end("<body style='text-align: center'><h1><b><u>404 Error</u></b></h1><h2>Page not found.  :(</h2></body>");
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      }
+    });
+  } else {
+    wss.handleUpgrade(req, req.socket, Buffer.alloc(0), function(ws) {
+      clients.add(ws);
+      console.log("1 Person joined the document.");
+      for(let client of clients) {
+        client.send(DATA);
+      }
+      ws.on('message', function(message) {
+        DATA = "" + message;
+        for(let client of clients) {
+          client.send(DATA);
+        }
+      });
+      ws.on('close', function() {
+        clients.delete(ws);
+        console.log("1 Person left the document.");
+      });
+    });
   }
-
-  ws.on('message', function(message) {
-    DATA = "" + message;
-    for(let client of clients) {
-      client.send(DATA);
-    }
-  });
-
-  ws.on('close', function() {
-    clients.delete(ws);
-    console.log("1 Person left the document.");
-  });
-
-}
+}).listen(80);
